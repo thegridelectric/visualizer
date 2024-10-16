4a20fbcc18a48a829c52373781ebb7034514d569
thdfw <th.defauw@gmail.com>
2024-10-15 17:01:01 -0700

    Single plot

--- rest_api.py
+++ rest_api.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import io
 import zipfile
 from fastapi.responses import StreamingResponse
+import pandas as pd
 
 settings = Settings(_env_file=dotenv.find_dotenv())
 valid_password = settings.thermostat_api_key.get_secret_value()
@@ -32,70 +33,40 @@ app.add_middleware(
 class DataRequest(BaseModel):
     password: str
 
-@app.post("/{house_alias}/thermostats")
-async def get_latest_temperature(house_alias: str, request: DataRequest):
+def to_fahrenheit(t):
+    return t*9/5+32
 
-    if request.password != valid_password:
-        raise HTTPException(status_code=403, detail="Unauthorized")
+# @app.post("/{house_alias}/thermostats")
+# async def get_latest_temperature(house_alias: str, request: DataRequest):
 
-    session = Session()
-    timezone = "America/New_York"
-    start = pendulum.datetime(2024, 1, 1, 0, 0, tz=timezone)
-    start_ms = int(start.timestamp() * 1000)
+#     if request.password != valid_password:
+#         raise HTTPException(status_code=403, detail="Unauthorized")
 
-    last_message = session.query(MessageSql).filter(
-        MessageSql.from_alias.like(f'%{house_alias}%'),
-        MessageSql.message_persisted_ms >= start_ms
-    ).order_by(desc(MessageSql.message_persisted_ms)).first()
+#     session = Session()
+#     timezone = "America/New_York"
+#     start = pendulum.datetime(2024, 1, 1, 0, 0, tz=timezone)
+#     start_ms = int(start.timestamp() * 1000)
 
-    if not last_message:
-        raise HTTPException(status_code=404, detail="No messages found.")
+#     last_message = session.query(MessageSql).filter(
+#         MessageSql.from_alias.like(f'%{house_alias}%'),
+#         MessageSql.message_persisted_ms >= start_ms
+#     ).order_by(desc(MessageSql.message_persisted_ms)).first()
 
-    temperature_data = []
-    for channel in last_message.payload['ChannelReadingList']:
-        if ('zone' in channel['ChannelName'] and 'gw' not in channel['ChannelName'] 
-            and ('temp' in channel['ChannelName'] or 'set' in channel['ChannelName'])):
-                temperature_data.append({
-                    "zone": channel['ChannelName'],
-                    "temperature": channel['ValueList'][-1] / 1000,
-                    "time": last_message.message_persisted_ms
-                })
+#     if not last_message:
+#         raise HTTPException(status_code=404, detail="No messages found.")
 
-    return temperature_data
+#     temperature_data = []
+#     for channel in last_message.payload['ChannelReadingList']:
+#         if ('zone' in channel['ChannelName'] and 'gw' not in channel['ChannelName'] 
+#             and ('temp' in channel['ChannelName'] or 'set' in channel['ChannelName'])):
+#                 temperature_data.append({
+#                     "zone": channel['ChannelName'],
+#                     "temperature": channel['ValueList'][-1] / 1000,
+#                     "time": last_message.message_persisted_ms
+#                 })
 
+#     return temperature_data
 
-@app.post("/{house_alias}")
-async def get_latest_temperature(house_alias: str, request: DataRequest, start_ms: int, end_ms: int):
-
-    if request.password != valid_password:
-        raise HTTPException(status_code=403, detail="Unauthorized")
-
-    session = Session()
-
-    messages = session.query(MessageSql).filter(
-        MessageSql.from_alias.like(f'%{house_alias}%'),
-        MessageSql.message_persisted_ms >= start_ms,
-        MessageSql.message_persisted_ms <= end_ms,
-    ).order_by(desc(MessageSql.message_persisted_ms)).all()
-
-    if not messages:
-        raise HTTPException(status_code=404, detail="No messages found.")
-
-    hp_odu_pwr = []
-    hp_idu_pwr = []
-    for message in messages:
-        for channel in message.payload['ChannelReadingList']:
-            if 'hp-odu-pwr' in channel['ChannelName']:
-                hp_odu_pwr.extend(channel['ValueList'])            
-            elif 'hp-idu-pwr' in channel['ChannelName']:
-                hp_idu_pwr.extend(channel['ValueList'])
-
-    hp_power_data = {
-        'hp_odu_pwr': hp_odu_pwr,
-        'hp_idu_pwr': hp_idu_pwr,
-    }
-
-    return hp_power_data
 
 @app.post('/{house_alias}/plots')
 async def get_plots(house_alias: str, request: DataRequest, start_ms: int, end_ms: int):
@@ -117,8 +88,6 @@ async def get_plots(house_alias: str, request: DataRequest, start_ms: int, end_m
     channels = {}
     for message in messages:
         for channel in message.payload['ChannelReadingList']:
-            if 'zone' in channel['ChannelName']:
-                continue
             if channel['ChannelName'] not in channels:
                 channels[channel['ChannelName']] = {
                     'values': channel['ValueList'],
@@ -129,48 +98,193 @@ async def get_plots(house_alias: str, request: DataRequest, start_ms: int, end_m
                 channels[channel['ChannelName']]['times'].extend(channel['ScadaReadTimeUnixMsList'])
 
     # Sort values according to time
-    first_time = 1e9**2
     for key in channels.keys():
         sorted_times_values = sorted(zip(channels[key]['times'], channels[key]['values']))
         sorted_times, sorted_values = zip(*sorted_times_values)
         channels[key]['times'] = list(sorted_times)
         channels[key]['values'] = list(sorted_values)
-        if channels[key]['times'][0] < first_time:
-            first_time = channels[key]['times'][0]
 
     # Create a BytesIO object for the zip file
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
-        for plot_type in ['pwr', 'wt', 'pipe']:
-            plt.figure(figsize=(12,4))
-            for key in channels.keys():
-                if plot_type not in key:
-                    continue
-                times = [(x - first_time) / 1000 / 60 for x in channels[key]['times']]
-                if max(times) > 120:
-                    times_hours = [x/60 for x in times]
-                values = [x / 1000 for x in channels[key]['values']] if plot_type != 'pwr' else channels[key]['values']
-                plt.plot(times, values, label=key) if max(times)<120 else plt.plot(times_hours, values, label=key)
-                plt.title(f'Starting at {pendulum.from_timestamp(first_time / 1000, tz="America/New_York").format("YYYY-MM-DD HH:mm:ss")}')
-            plt.xlabel('Time [min]') if max(times)<120 else plt.xlabel('Time [hours]')
-            if plot_type=='pwr':
-                plt.ylabel('Power [W]')
-            elif plot_type=='wt':
-                plt.ylabel('Temperature [C]')
-            elif plot_type=='pipe':
-                plt.ylabel('Temperature [C]')
-            plt.legend()
 
-            # Save the plot to a BytesIO object
-            img_buf = io.BytesIO()
-            plt.savefig(img_buf, format='png')
-            img_buf.seek(0)  # Move to the beginning of the BytesIO object
-            
-            # Write the image to the zip file
-            zip_file.writestr(f'{plot_type}_plot.png', img_buf.getvalue())
-            plt.close()
+        fig, ax = plt.subplots(5,1, figsize=(12,20), sharex=True)
+        
+        # --------------------------------------
+        # First plot: HP loop
+        # --------------------------------------
 
-    zip_buffer.seek(0)  # Move to the beginning of the BytesIO object
+        first_plot_keys = []
+        for key in channels.keys():
+
+            if 'hp' in key or 'primary' in key:
+                first_plot_keys.append(key)
+                channels[key]['values'] = [x/1000 for x in channels[key]['values']]
+                channels[key]['times'] = [x/1000 for x in channels[key]['times']]
+                channels[key]['times'] = pd.to_datetime(channels[key]['times'], unit='s')
+                channels[key]['times'] = channels[key]['times'].tz_localize('America/New_York')
+                if 'wt' in key:
+                    channels[key]['values'] = [to_fahrenheit(x) for x in channels[key]['values']]
+
+        ax[0].plot(channels['hp-lwt']['times'], channels['hp-lwt']['values'], color='tab:red', alpha=0.7, label='HP LWT')
+        ax[0].plot(channels['hp-ewt']['times'], channels['hp-ewt']['values'], color='tab:blue', alpha=0.7, label='HP EWT')
+        ax[0].set_ylabel('Temperature [F]')
+        if max(channels['hp-lwt']['values']) < 190:
+            ax[0].set_ylim([0,190])
+        ax[0].legend(loc='upper left')
+        ax20 = ax[0].twinx()
+        ax20.plot(channels['hp-odu-pwr']['times'], channels['hp-odu-pwr']['values'], color='tab:green', alpha=0.7, label='HP outdoor')
+        ax20.plot(channels['hp-idu-pwr']['times'], channels['hp-idu-pwr']['values'], color='orange', alpha=0.7, label='HP indoor')
+        ax20.plot(channels['primary-pump-pwr']['times'], channels['primary-pump-pwr']['values'], color='purple', alpha=0.7, label='Primary pump')
+        ax20.set_ylabel('Power [kW]')
+        if max(channels['hp-odu-pwr']['values']) < 30 and max(channels['hp-idu-pwr']['values']) < 30:
+            ax20.set_ylim([0,30])
+        ax20.legend(loc='upper right')
+        ax[0].set_title('Heat pump')
+
+        # --------------------------------------
+        # Second plot: distribution loop
+        # --------------------------------------
+
+        second_plot_keys = []
+        for key in channels.keys():
+
+            if 'dist' in key: #or 'zone' in key:
+                second_plot_keys.append(key)
+                if 'pump' not in key and 'zone' not in key:
+                    channels[key]['values'] = [x/1000 for x in channels[key]['values']]
+                channels[key]['times'] = [x/1000 for x in channels[key]['times']]
+                channels[key]['times'] = pd.to_datetime(channels[key]['times'], unit='s')
+                channels[key]['times'] = channels[key]['times'].tz_localize('America/New_York')
+                if 'wt' in key:
+                    channels[key]['values'] = [to_fahrenheit(x) for x in channels[key]['values']]
+                
+        ax[1].plot(channels['dist-swt']['times'], channels['dist-swt']['values'], color='tab:red', alpha=0.7, label='Distribution SWT')
+        ax[1].plot(channels['dist-rwt']['times'], channels['dist-rwt']['values'], color='tab:blue', alpha=0.7, label='Distribution RWT')
+        ax[1].set_ylabel('Temperature [F]')
+        ax[1].set_ylim([0,190])
+        ax[1].legend(loc='upper left')
+        ax21 = ax[1].twinx()
+        ax21.plot(channels['dist-pump-pwr']['times'], channels['dist-pump-pwr']['values'], color='tab:green', alpha=0.7, label='Distribution pump')
+        # for key in channels:
+        #     if 'zone' in key and 'pwr' in key:
+        #         ax2.plot(channels[key]['times'], channels[key]['values'], alpha=0.7, label=key)
+        ax21.set_ylabel('Power [W]')
+        ax21.set_ylim([0,40])
+        ax21.legend(loc='upper right')
+        ax[1].set_title('Distribution')
+
+        # --------------------------------------
+        # Third plot: zones
+        # --------------------------------------
+
+        third_plot_keys = []
+        for key in channels.keys():
+
+            if 'zone' in key:
+                third_plot_keys.append(key)
+                channels[key]['values'] = [x/1000 for x in channels[key]['values']]
+                channels[key]['times'] = [x/1000 for x in channels[key]['times']]
+                channels[key]['times'] = pd.to_datetime(channels[key]['times'], unit='s')
+                channels[key]['times'] = channels[key]['times'].tz_localize('America/New_York')
+
+        zones = {}
+        for key in third_plot_keys:
+            if key.split('-')[0] not in zones:
+                zones[key.split('-')[0]] = [key]
+            else:
+                zones[key.split('-')[0]].append(key)
+
+        ax22 = ax[2].twinx()
+        colors = {}
+        for zone in zones:
+            for temp in zones[zone]:
+                if 'temp' in temp:
+                    color = ax[2].plot(channels[temp]['times'], channels[temp]['values'], label=temp, alpha=0.7)[0].get_color()
+                    colors[temp] = color
+                elif 'set' in temp:
+                    base_temp = temp.replace('-set', '-temp')
+                    if base_temp in colors:
+                        ax22.plot(channels[temp]['times'], channels[temp]['values'], label=temp, 
+                                linestyle='dashed', color=colors[base_temp], alpha=0.7)
+        ax[2].set_ylabel('Temperature [F]')
+        ax22.set_ylabel('Setpoint [F]')
+        ax[2].set_ylim([55, 75])
+        ax22.set_ylim([55, 75])
+        ax[2].legend(loc='upper left')
+        ax22.legend(loc='upper right')
+        ax[2].set_title('Zones')
+
+        # --------------------------------------
+        # Fourth plot: buffer
+        # --------------------------------------
+
+        fourth_plot_keys = []
+        for key in channels.keys():
+            if 'buffer' in key:
+                fourth_plot_keys.append(key)
+                if 'pipe' in key:
+                    channels[key]['values'] = [to_fahrenheit(x/1000) for x in channels[key]['values']]
+                else:
+                    channels[key]['values'] = [x/1000 for x in channels[key]['values']]
+                channels[key]['times'] = [x/1000 for x in channels[key]['times']]
+                channels[key]['times'] = pd.to_datetime(channels[key]['times'], unit='s')
+                channels[key]['times'] = channels[key]['times'].tz_localize('America/New_York')
+
+        ax[3].plot(channels['buffer-hot-pipe']['times'], channels['buffer-hot-pipe']['values'], 
+                color='tab:red', alpha=0.7, label='Buffer hot pipe')
+        ax[3].plot(channels['buffer-cold-pipe']['times'], channels['buffer-cold-pipe']['values'], 
+                color='tab:blue', alpha=0.7, label='Buffer cold pipe')
+        ax[3].set_ylabel('Temperature [F]')
+        ax[3].set_ylim([40,190])
+        ax[3].legend(loc='upper left')
+        ax[3].set_title('Buffer')
+
+        # --------------------------------------
+        # Fifth plot: storage
+        # --------------------------------------
+
+        fifth_plot_keys = []
+        for key in channels.keys():
+            if 'store' in key:
+                fifth_plot_keys.append(key)
+                if 'pipe' in key:
+                    channels[key]['values'] = [to_fahrenheit(x/1000) for x in channels[key]['values']]
+                else:
+                    channels[key]['values'] = [x/1000 for x in channels[key]['values']]
+                channels[key]['times'] = [x/1000 for x in channels[key]['times']]
+                channels[key]['times'] = pd.to_datetime(channels[key]['times'], unit='s')
+                channels[key]['times'] = channels[key]['times'].tz_localize('America/New_York')
+
+        ax[4].plot(channels['store-hot-pipe']['times'], channels['store-hot-pipe']['values'], 
+                color='tab:red', alpha=0.7, label='Storage hot pipe')
+        ax[4].plot(channels['store-cold-pipe']['times'], channels['store-cold-pipe']['values'], 
+                color='tab:blue', alpha=0.7, label='Storage cold pipe')
+        ax[4].set_ylabel('Temperature [F]')
+        ax[4].set_ylim([40,190])
+        ax[4].legend(loc='upper left')
+        ax24 = ax[4].twinx()
+        ax24.plot(channels['store-pump-pwr']['times'], channels['store-pump-pwr']['values'], 
+                color='tab:green', alpha=0.7, label='Storage pump')
+        ax24.set_ylabel('Power [W]')
+        ax24.set_ylim([0,40])
+        ax24.legend(loc='upper right')
+        ax[4].set_title('Storage')
+
+
+        ax[0].tick_params(axis='x', which='both', labelbottom=True)
+        ax[1].tick_params(axis='x', which='both', labelbottom=True)
+        ax[2].tick_params(axis='x', which='both', labelbottom=True)
+        ax[3].tick_params(axis='x', which='both', labelbottom=True)
+        ax[4].tick_params(axis='x', which='both', labelbottom=True)
+
+        img_buf = io.BytesIO()
+        plt.savefig(img_buf, format='png')
+        img_buf.seek(0)
+        zip_file.writestr(f'plot.png', img_buf.getvalue())
+        plt.close()
+
+    zip_buffer.seek(0)
     return StreamingResponse(zip_buffer, media_type='application/zip', headers={"Content-Disposition": "attachment; filename=plots.zip"})
 
 
