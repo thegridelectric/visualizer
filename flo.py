8230798dcdfc0c8bc6ccf0e054ff2c85fe13d0b1
thdfw <th.defauw@gmail.com>
2025-01-19 11:51:39 +0100

    Solve download of the wrong excel file

--- flo.py
+++ flo.py
@@ -510,7 +510,8 @@ class DGraph():
         # Write to Excel
         os.makedirs('results', exist_ok=True)
         start = datetime.fromtimestamp(self.params.start_time, tz=pytz.timezone("America/New_York")).strftime('%Y-%m-%d %H:%M')
-        file_path = os.path.join('results', f'result_{start}.xlsx')
+        # file_path = os.path.join('results', f'result_{start}.xlsx')
+        file_path = 'result.xlsx'
         with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
             results_df.to_excel(writer, index=False, sheet_name='Pathcost')
             results_df.to_excel(writer, index=False, sheet_name='Next node')
