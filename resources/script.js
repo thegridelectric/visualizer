78844a5fdf1f6af033a6b9296e8c03904b0a0114
thdfw <th.defauw@gmail.com>
2025-03-09 12:43:15 +0100

    WIP price editor

--- resources/script.js
+++ resources/script.js
@@ -1,8 +1,8 @@
 let house_alias, username, password;
 let darkmode_tf = false;
 let plotsDisplayed = false;
-// let api_host = 'http://localhost:8000'
-let api_host = 'https://visualizer.electricity.works'
+let api_host = 'http://localhost:8000'
+// let api_host = 'https://visualizer.electricity.works'
 
 function clearPlots() {
     const plotDivs = [
