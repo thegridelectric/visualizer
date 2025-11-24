f674236553ca2d817017fc698932be234ad7de6d
thdfw <th.defauw@gmail.com>
2025-11-24 15:35:12 +0100

    Adding start websockets script

--- start_websockets.sh
+++ start_websockets.sh
@@ -5,10 +5,13 @@ sessions=("beech" "oak" "fir" "maple" "elm")
 
 # Start websocket servers in tmux sessions
 for session in "${sessions[@]}"; do
-    tmux new-session -d -s "$session"
-    tmux send-keys -t "$session" "gw" C-m
-    tmux send-keys -t "$session" "cd ${session}-webinter" C-m
-    tmux send-keys -t "$session" "./start_websocket_server.sh" C-m
+    # Check if session already exists
+    if ! tmux has-session -t "$session" 2>/dev/null; then
+        tmux new-session -d -s "$session"
+        tmux send-keys -t "$session" "gw" C-m
+        tmux send-keys -t "$session" "cd ${session}-webinter" C-m
+        tmux send-keys -t "$session" "./start_websocket_server.sh" C-m
+    fi
 done
 
 echo "Started all websocket servers in tmux sessions:"
